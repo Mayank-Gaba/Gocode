65debad028b7afc65397358c90010c5b81a8ca92
Mayank Gaba <cu.17bcs1192@gmail.com>
2020-02-03 01:44:15 +0530

    Template for competitive coding

---
+++ Template_for_competitive_coding.cpp
@@ -0,0 +1,29 @@
+// live today - love tomorrow - unite forever // 
+#include<bits/stdc++.h>
+using namespace std;
+#define io ios::sync_with_stdio(false);cin.tie(NULL);
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+long int mod=1000000007;
+#define endl "\n"
+int main()
+{
+	io
+	#ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin); 
+    freopen("output.txt", "w", stdout);
+	#endif 
+
+    int t=1;
+    cin>>t;
+
+    while(t--)
+    {
+
+    }
+}
+// Thank you - Good Night// 
